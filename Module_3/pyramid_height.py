668f6bb8898d62bb927f11e3d66c369d5399a6ce
trendswave <paulchinedumicheal@gmail.com>
2023-10-22 15:34:41 +0100

    a program which reads the number of blocks the builders have, and outputs the height of the pyramid that can be built using these blocks.

---
+++ Module_3/pyramid_height.py
@@ -0,0 +1,12 @@
+#Prompt user to input number of blocks
+block = int(input("Enter the numnber of blocks")
+#initialize block height to zero
+height = 0
+#initialise block layer to 1
+layer_block = 1
+#while block is greater or equal to layer_blocks
+while block >= layer_block
+height +=1
+blocks -= layer_blocks
+layer_blocks += 1
+print("The height of the pyramid is:" ,height)
